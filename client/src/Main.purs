1382fa470558232d2f2b9404579c8a50e3fd3934
Simon Olander <simon.olander@r2m.se>
2021-03-19 12:38:24 +0100

    Update ci pipeline

--- client/src/Main.purs
+++ client/src/Main.purs
@@ -2,9 +2,12 @@ module Main where
 
 import Prelude
 
+import App.Button as Button
 import Effect (Effect)
-import Effect.Console (log)
+import Halogen.Aff as HA
+import Halogen.VDom.Driver (runUI)
 
 main :: Effect Unit
-main = do
-  log "🍝"
+main = HA.runHalogenAff do
+  body <- HA.awaitBody
+  runUI Button.component unit body
