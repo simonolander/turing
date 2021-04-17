f0130dea88054bb4af92cd2a3030111d05265d40
Simon Olander <simon.olander@r2m.se>
2021-04-17 09:28:43 +0200

    Add quickcheck tests to CardId

--- client/test/Turing/Data/CardId.purs
+++ client/test/Turing/Data/CardId.purs
@@ -9,10 +9,15 @@ import Test.Unit
 import Test.Unit.Assert
 import Turing.Data.CardId
 import Data.Newtype
-import Data.Show
+import Data.Show (show)
+import Test.Unit.QuickCheck
 
 main :: Effect Unit
 main = runTest do
     suite "CardId" do
         test "show" do
-            equal "A" (show (CardId "A"))
+            quickCheck \id -> id == (show (CardId id))
+        test "wrap" do
+            quickCheck \id -> wrap id == (CardId id)
+        test "eq" do
+            quickCheck \id1 id2 -> (id1 == id2) == ((CardId id1) == (CardId id2))
