7751ed8fa8894b18a7df9e51b1a031b9ff82337f
Simon Olander <simon.olander@r2m.se>
2021-04-17 11:52:28 +0200

    Add tests for sub step scanning

--- client/src/Turing/Data/Program.purs
+++ client/src/Turing/Data/Program.purs
@@ -1,10 +1,17 @@
 module Turing.Data.Program where
 
-import Turing.Data.CardDeck (CardDeck)
+import Turing.Data.CardDeck (CardDeck, empty)
 import Turing.Data.Card (Card)
 
 type Program =
     { deck :: CardDeck
     , initialCard :: Card
     , initialPosition :: Int
+    }
+
+program :: Card -> Program
+program initialCard =
+    { deck: empty
+    , initialCard
+    , initialPosition: 0
     }
--- client/test/Main.purs
+++ client/test/Main.purs
@@ -6,8 +6,10 @@ import Effect (Effect)
 
 import Test.Turing.Data.CardId as CardId
 import Test.Turing.Data.Tape as Tape
+import Test.Turing.Data.Execution as Execution
 
 main :: Effect Unit
 main = do
     CardId.main
     Tape.main
+    Execution.main
