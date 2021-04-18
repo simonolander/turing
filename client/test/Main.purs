02684f571f1bd44bc56495a9d8e6b5f23dc46839
Simon Olander <simon.olander@r2m.se>
2021-04-18 10:11:52 +0200

    Add skeleton for random test

--- client/test/Main.purs
+++ client/test/Main.purs
@@ -7,9 +7,11 @@ import Effect (Effect)
 import Test.Turing.Data.CardId as CardId
 import Test.Turing.Data.Tape as Tape
 import Test.Turing.Data.Execution as Execution
+import Test.Turing.Utils.Random as Random
 
 main :: Effect Unit
 main = do
     CardId.main
     Tape.main
     Execution.main
+    Random.main
