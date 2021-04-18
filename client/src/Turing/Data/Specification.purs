d3d3675bd0a04b1f496198b34b0c75841165761a
Simon Olander <simon.olander@r2m.se>
2021-04-18 14:53:59 +0200

    fixup! Specification page

--- client/src/Turing/Data/Specification.purs
+++ client/src/Turing/Data/Specification.purs
@@ -3,8 +3,23 @@ module Turing.Data.Specification where
 
 import Prelude
 import Turing.Data.Tape as Tape
+import Data.Eq (class Eq)
+import Data.Ord (class Ord)
+import Data.Newtype (class Newtype, unwrap, wrap)
+import Data.Show (class Show)
+import Data.Maybe
 
-type SpecificationId = String
+newtype SpecificationId = SpecificationId String
+
+derive instance eqSpecificationId :: Eq SpecificationId
+derive instance ordSpecificationId :: Ord SpecificationId
+derive instance newtypeSpecificationId :: Newtype SpecificationId _
+instance showSpecificationId :: Show SpecificationId where
+    show = unwrap
+
+parseId :: String -> Maybe SpecificationId
+parseId "" = Nothing
+parseId str = Just $ wrap str
 
 type Specification =
     { id :: SpecificationId
