99070fdd87a31113f5dc774c5adf7024131bf76d
Simon Olander <simon.olander@r2m.se>
2021-04-18 10:22:31 +0200

    Add random test

--- client/test/Turing/Utils/Random.purs
+++ client/test/Turing/Utils/Random.purs
@@ -3,17 +3,18 @@ module Test.Turing.Utils.Random where
 import Prelude (Unit, bind, pure, ($), (==))
 
 import Effect (Effect)
-import Effect.Class.Console (log)
+import Effect.Class (liftEffect)
 import Test.Unit.Main (runTest)
 import Test.Unit
 import Turing.Utils.Random
 import Test.Unit.QuickCheck (quickCheck)
 import Data.String (length)
 import Effect.Unsafe (unsafePerformEffect)
+import Test.Unit.Assert (equal)
 
 main :: Effect Unit
 main = runTest do
     suite "Random" do
         test "random string length" do
-            quickCheck \(len :: Int) ->
-                true -- TODO
+            string <- liftEffect $ randomString 6
+            equal 6 $ length string
