5e8fb29eb730abffb6aaaa88b3c95c2aa8a55cbf
Simon Olander <simon.olander@r2m.se>
2021-04-29 21:27:27 +0200

    Update page template

--- client/src/Turing/Page/Template.purs
+++ client/src/Turing/Page/Template.purs
@@ -3,6 +3,8 @@ module Turing.Page.Template where
 import Prelude
 import Halogen as H
 import Halogen.HTML as HH
+import Turing.AppM (AppM)
+import Data.Const (Const)
 
 type State = Unit
 
@@ -11,18 +13,21 @@ type Action = Unit
 type Slots :: forall k. Row k
 type Slots = ()
 
+type Query :: forall k. k -> Type
+type Query = Const Unit
+
 type Input = Unit
 
 type Output = Void
 
-component :: forall query m. H.Component query Input Output m
+component :: H.Component Query Input Output AppM
 component = H.mkComponent { initialState, render, eval }
     where
     initialState :: Input -> State
     initialState = const unit
 
-    render :: State -> HH.HTML (H.ComponentSlot Slots m Action) Action
+    render :: State -> HH.HTML (H.ComponentSlot Slots AppM Action) Action
     render = const $ HH.h1_ [ HH.text "Template" ]
 
-    eval :: H.HalogenQ query Action Input ~> H.HalogenM State Action Slots Output m
+    eval :: H.HalogenQ Query Action Input ~> H.HalogenM State Action Slots Output AppM
     eval = H.mkEval H.defaultEval
