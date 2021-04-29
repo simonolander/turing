c822c685c24da38cc5a8f0c506fcec2250e36e03
Simon Olander <simon.olander@r2m.se>
2021-04-29 22:30:27 +0200

    Add random string function

---
+++ client/src/Turing/Effect/Random.purs
@@ -0,0 +1,22 @@
+module Turing.Effect.Random (randomString) where
+
+import Prelude
+import Effect (Effect)
+import Effect.Random (randomInt)
+import Data.String (length, codePointAt, fromCodePointArray)
+import Data.Maybe (fromMaybe)
+import Data.String.CodePoints (CodePoint, codePointFromChar)
+import Data.Unfoldable (replicateA)
+
+alphabet :: String
+alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz012345689"
+
+randomCodePoint :: Effect CodePoint
+randomCodePoint = do
+    index <- randomInt 0 (length alphabet - 1)
+    pure $ fromMaybe (codePointFromChar '_') $ codePointAt index alphabet
+
+randomString :: Int -> Effect String
+randomString length = do
+    codePoints <- replicateA length randomCodePoint
+    pure $ fromCodePointArray codePoints
