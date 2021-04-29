6d0bea08d41e400b14615e222e23666f6ce33d20
Simon Olander <simon.olander@r2m.se>
2021-04-29 18:32:30 +0200

    Remove commented out code

--- client/src/Turing/Component/Form/Spec.purs
+++ client/src/Turing/Component/Form/Spec.purs
@@ -1,4 +1,4 @@
-module Turing.Component.Form.Spec where
+module Turing.Component.Form.Spec (Slot, SpecForm, component) where
 
 import Prelude
 import Data.Const (Const)
@@ -26,8 +26,6 @@ derive instance newtypeSpecForm :: Newtype (SpecForm r f) _
 
 type Slot = H.Slot (F.Query SpecForm (Const Void) ()) Spec
 
---data Action = HandleSpec Spec
-
 component :: forall m.
     MonadEffect m =>
     MonadAff m =>
@@ -50,38 +48,11 @@ component = F.component (const formInput) $ F.defaultSpec { render = renderForml
             [ HH.input []
             , HH.textarea []
             , HH.button
-                [ HE.onClick \_ -> F.submit ]
+                [ HE.onClick $ const F.submit ]
                 [ HH.text "Submit" ]
             ]
         where
         _name = Proxy :: Proxy "name"
         _text = Proxy :: Proxy "maxNumberOfCards"
-
---component :: forall m.
---  MonadEffect m =>
---  MonadAff m =>
---  H.Component (Const Void) Unit Void m
---component = H.mkComponent
---  { initialState: const unit
---  , render: const render
---  , eval: H.mkEval $ H.defaultEval { handleAction = handleAction }
---  }
---  where
---  handleAction = case _ of
---  HandleSpec contact -> H.liftEffect $ logShow (contact :: Spec)
---
---  render =forall m.
-      ----  MonadEffect m =>
-      ----  MonadAff m =>
---  HH.section_
---    [ HH.h1_ [ HH.text "Formless" ]
---    , HH.h2_ [ HH.text "A basic contact form." ]
---    , HH.text
---      """
---      You can create a full Halogen contact form like this in less than 20 lines of Formless, excluding the render function.  It's type-safe, supports complex types, has validation, and parses to the output type of your choice."
---      """
---    , HH.br_
---    , HH.slot F._formless unit formComponent unit HandleSpec
---    ]
 
 
