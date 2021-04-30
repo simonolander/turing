49c451c247d11e87532db2fec6b62798fb635bd3
Simon Olander <simon.olander@r2m.se>
2021-04-30 17:45:42 +0200

    Add get spec function

--- client/src/Turing/Capability/ManageSpec.purs
+++ client/src/Turing/Capability/ManageSpec.purs
@@ -3,10 +3,11 @@ module Turing.Capability.ManageSpec where
 import Prelude
 import Control.Monad.Reader.Trans (lift)
 import Halogen (HalogenM)
-import Turing.Data.Route (Route)
+import Turing.Data.Spec (Spec, SpecId)
+import Data.Maybe (Maybe)
 
 class Monad m <= ManageSpec m where
-    manageSpec :: Route -> m Unit
+    getSpec :: SpecId -> m (Maybe Spec)
 
 instance manageSpecHalogenM :: ManageSpec m => ManageSpec ( HalogenM state action slots output m ) where
-    manageSpec = lift <<< manageSpec
+    getSpec = lift <<< getSpec
