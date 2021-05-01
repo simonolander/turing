416e159ff857b4c2809770c9e8311cb7bcacddc4
Simon Olander <simon.olander@r2m.se>
2021-05-01 14:44:47 +0200

    Render different cases of spec

--- client/src/Turing/Page/SpecEditor.purs
+++ client/src/Turing/Page/SpecEditor.purs
@@ -11,7 +11,7 @@ import Data.Const (Const)
 import Formless as F
 import Effect.Console (infoShow)
 import Data.Maybe (Maybe(..))
-import Network.RemoteData (RemoteData(..))
+import Network.RemoteData (RemoteData(..), fromEither)
 import Foreign (MultipleErrors)
 
 type State =
@@ -42,8 +42,18 @@ component = H.mkComponent { initialState, render, eval }
     render :: State -> HH.HTML (H.ComponentSlot Slots AppM Action) Action
     render state =
         HH.div_
-            [ HH.h1_ [ HH.text "Edit spec" ]
-            , HH.slot F._formless unit SF.component state.specId HandleSpecForm
+            [ HH.h1_ [ HH.text $ "Spec " <> state.specId ]
+            , case state.spec of
+                NotAsked -> HH.text "Not asked"
+                Loading -> HH.text "Loading spec"
+                Failure error -> HH.text (show error)
+                Success (Nothing) ->
+                    HH.div_
+                        [ HH.h2_ [ HH.text "404 Not found" ]
+                        , HH.p_ [ HH.text $ "Spec " <> state.specId <> " does not exists." ]
+                        ]
+                Success (Just spec) ->
+                    HH.slot F._formless unit SF.component state.specId HandleSpecForm
             ]
 
     eval :: H.HalogenQ Query Action Input ~> H.HalogenM State Action Slots Output AppM
@@ -55,6 +65,7 @@ component = H.mkComponent { initialState, render, eval }
         handleAction :: Action -> H.HalogenM State Action Slots Output AppM Unit
         handleAction Initialize = do
             specId <- H.gets _.specId
+            H.modify_ _ { spec = Loading }
             eitherSpec <- getSpec specId
-            pure unit
+            H.modify_ _ { spec = fromEither eitherSpec}
         handleAction (HandleSpecForm spec) = H.liftEffect $ infoShow spec
